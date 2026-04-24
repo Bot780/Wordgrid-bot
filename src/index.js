79dcfd3da86a04a71443799f954a6f9d82087eb8
Levi00117 <Ansariwais130@gmail.com>
2026-04-24 12:55:51 +0530

    ssss

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 app.get('/', (_, res) => res.send('Bot alive'));
 app.listen(process.env.PORT || 3000, () =>
-console.log('🌐 Server running on port ${process.env.PORT || 3000}');
+console.log(`🌐 Server running on port ${process.env.PORT || 3000}`);
 );
 
 const {
