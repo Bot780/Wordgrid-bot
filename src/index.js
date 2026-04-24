37d4cbd51b1730aab727abf85789443fd7e4aa2e
Levi00117 <Ansariwais130@gmail.com>
2026-04-24 13:02:30 +0530

    lg

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 app.get('/', (_, res) => res.send('Bot alive'));
-app.listen(process.env.PORT || 3000, () =>
-console.log(`🌐 Server running on port ${process.env.PORT || 3000}`);
-);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`🌐 Server running on port ${process.env.PORT || 3000}`);
+});
 
 const {
 Client,
